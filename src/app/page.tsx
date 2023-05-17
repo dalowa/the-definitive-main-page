7a92cbe86d0b050e0d909f9c2728a0cb617464cb
dalowa <david.developer.work@gmail.com>
2023-05-16 19:49:31 -0500

    Metadata improved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-"use client";
-
 import UltimateSearchBar from "@/components/UltimateSearchBar";
-import Head from "next/head";
+
+export const metadata = {
+  title: "The Definitive Main Page",
+  description: "Dalowa's project",
+};
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>The Definitive Main Page</title>
-      </Head>
       <main>
         <section className="w-full flex justify-center items-center py-5 sm:py-7 md:p-10">
           <UltimateSearchBar />

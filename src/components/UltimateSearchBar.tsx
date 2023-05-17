a41d3baa57fc8ace01c51c325eab02d8602f478a
dalowa <david.developer.work@gmail.com>
2023-05-16 21:27:35 -0500

    now its a form

--- src/components/UltimateSearchBar.tsx
+++ src/components/UltimateSearchBar.tsx
@@ -40,17 +40,49 @@ export default function UltimateSearchBar() {
     setInputValue(event.target.value);
   };
 
+  const handleSubmit = () => {
+    // Prevenir comportamiento por defecto (enviar formulario)
+    let transformed;
+    if (Object.keys(options)[index] == "ECOSIA") {
+      transformed = inputValue
+        .toLowerCase()
+        .replace(/ /g, "%20")
+        .replace(/[^a-zA-Z0-9%]/g, "");
+    }
+    if (Object.keys(options)[index] !== "ECOSIA") {
+      transformed = inputValue
+        .toLowerCase()
+        .replace(/ /g, "+")
+        .replace(/[^a-zA-Z0-9+]/g, "");
+    }
+
+    window.open(
+      `${options[Object.keys(options)[index]]}${transformed}`,
+      "_blank"
+    );
+    setInputValue("");
+    return;
+  };
+
   return (
     <div className="flex h-44 w-full max-w-sm sm:max-w-2xl md:max-w-4xl">
       <TheSelect options={Object.keys(options)} />
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-        placeholder="Search..."
-        className="py-0 px-2 h-10 focus:outline-none border-2 border-sky-700 w-2/3 md:text-2xl md:h-14"
-      />
+      <form className="w-2/3" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          /* onKeyDown={handleKeyDown} */
+          placeholder="Search..."
+          className="py-0 px-2 h-10 focus:outline-none border-2 border-sky-700 md:text-2xl md:h-14"
+        />
+        <button
+          type="submit"
+          className=" py-0 px-2 h-14 text-2xl border-2 border-sky-700 bg-sky-700"
+        >
+          🔎
+        </button>
+      </form>
     </div>
   );
 }

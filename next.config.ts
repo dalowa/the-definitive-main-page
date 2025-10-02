72668cbdb608a18c468d8d0194a824d593b60d45
dalowa <davidurbano.dev@gmail.com>
2025-10-02 13:55:47 -0500

    now is updated

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
+}
+
+module.exports = nextConfig
